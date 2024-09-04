b22fd0cece457ecce084a61a7f46fd8442ce3380
Benit0Trdlla <benilorenzo007op@gmail.com>
2024-09-04 15:52:51 -0300

    Add new technology to the latest proyect.

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -97,6 +97,7 @@ export const DATA = {
         "Este proyecto consistió en clonar la página web de la AFIP, la institución tributaria de Argentina, con el objetivo de mejorar mis habilidades como desarrollador frontend. Durante su desarrollo, me enfoqué en practicar y perfeccionar el uso de herramientas clave como React y Bootstrap, asegurando que el diseño y la funcionalidad fueran lo más fieles posibles al sitio original.",
       technologies: [
         "React",
+        "React Router",
         "CSS",
         "Bootstrap",
         "Vite",
