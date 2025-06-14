47fdc3dd8ffdaed605e00b75acf0d0cf39005f8d
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-06-14 19:01:59 -0300

    New modified text with focus on ‘product-minded developer’.

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -10,7 +10,7 @@ export const DATA = {
   description:
     "Desarrollador Frontend enfocado en crear interfaces modernas, eficientes y de alto rendimiento, utilizando mi conocimiento y mejores prácticas.",
   summary:
-    "Técnico Informático y Desarrollador Frontend con experiencia en HTML5, CSS3, JavaScript, React, Styled Components, Componentes reutilizables, Bootstrap y Next.js. Conocimientos sólidos en el desarrollo e integración de APIs RESTful, y utilizo Git para el control de versiones. Siempre abierto a aprender y adaptarme a nuevas tecnologías y entornos de desarrollo.",
+    "Técnico Informático y Desarrollador Frontend con experiencia en HTML5, CSS3, JavaScript, React, Styled Components, Componentes reutilizables, Bootstrap y Next.js. Conocimientos sólidos en el desarrollo e integración de APIs REST, y utilizo Git para el control de versiones. Siempre abierto a aprender y adaptarme a nuevas tecnologías y entornos de desarrollo.",
   avatarUrl: "/me.png",
   skills: [
     "React",
@@ -21,7 +21,7 @@ export const DATA = {
     "Styled Components",
     "Componentes reutilizables",
     "Bootstrap",
-    "API RESTful",
+    "API REST",
     // "NPM",
     "Git",
   ],
@@ -63,7 +63,7 @@ export const DATA = {
       logoUrl: "https://caribeensena.org/_assets/media/ea95eb4d8a32e4352ed2403a067facf8.png",
       start: "Marzo 2024",
       end: "Actualidad",
-      description: "● Desarrollo de la plataforma e-learning 'Caribe Enseña Learning', que ofrece cursos, talleres y diplomados.\n\n● Participación en la creación de una aplicación web progresiva (PWA) llamada ‘Proyecto Genius’. Aumento del acceso educativo en áreas con baja conectividad, gracias a la funcionalidad offline de la PWA.\n\n● Implementé y optimicé la experiencia de usuario (UX) y la interfaz de usuario (UI) de 'Proyecto Genius' y ‘Caribe Enseña Learning’. Reduciendo el tiempo de carga de la plataforma, lo que mejoró la retención de usuarios."
+      description: "● Desarrollo de la plataforma e-learning 'Caribe Enseña Learning', orientada a ofrecer cursos, talleres y diplomados con una navegación clara y accesible.\n\n● Participación en la creación de una aplicación web progresiva (PWA) llamada ‘Proyecto Genius’. Diseñada para ampliar el acceso educativo en zonas con baja conectividad, gracias a su funcionalidad offline.\n\n● Implementé y optimicé la UX/UI de 'Proyecto Genius' y ‘Caribe Enseña Learning’. Reduciendo el tiempo de carga de las plataformas, lo que mejoró la retención de usuarios."
     },
     {
       company: "Laboratorio Consultar SRL",
@@ -75,7 +75,7 @@ export const DATA = {
       start: "Mayo 2023",
       end: "Julio 2023",
       description:
-        "● Desarrollo del primer chatbot con IA de la empresa, reduciendo el tiempo de respuesta a las consultas de los clientes y automatizando las interacciones con el soporte.\n\n● Implementación de un sistema de agenda automatizado para la gestión de reuniones. Reduciendo la carga administrativa relacionada con la gestión de reuniones.\n\n● Desarrollo de una aplicación que automatizó el traslado de facturas a hojas de Excel, reduciendo y evitando errores manuales en el proceso de recopilación y transferencia de datos. Aumentando la eficiencia operativa, permitiendo al equipo enfocarse en tareas de mayor valor.",
+        "● Desarrollo del primer chatbot con IA de la empresa, utilizando la API de OpenAI y documentos técnicos propios. Automatizando respuestas a consultas frecuentes y reduciendo significativamente los tiempos de atención al cliente.\n\n● Implementación de un sistema de agenda automatizado, incluido en el propio chatbot, para la gestión de reuniones. Disminuyendo la carga administrativa.\n\n● Desarrollo de una aplicación para automatizar la exportación de facturas a Excel, eliminando errores manuales y aumentando la eficiencia operativa, lo que permitió al equipo enfocarse en tareas de mayor valor.",
     },
   ],
   education: [
@@ -95,7 +95,7 @@ export const DATA = {
       dates: "Septiembre 2024 - Octubre 2024",
       active: true,
       description:
-        "Asistente educativo con IA, diseñado para apoyar a estudiantes en diversas materias escolares. Responde preguntas, proporciona explicaciones claras y ofrece ejemplos personalizados. Una funcionalidad clave del asistente es la capacidad de subir imágenes para facilitar la comprensión de temas complejos.",
+        "Asistente educativo impulsado por IA, creado para acompañar a estudiantes en su aprendizaje diario. Responde preguntas, explica conceptos de forma clara y brinda ejemplos adaptados a cada necesidad. Además, permite subir imágenes para analizar y comprender mejor temas complejos.",
       technologies: [
         "Next.js",
         "React",
@@ -117,7 +117,7 @@ export const DATA = {
       dates: "Septiembre 2024 - Septiembre 2024",
       active: true,
       description:
-        "Genera y gestiona tus contraseñas de forma segura. Puedes crearlas automáticamente o personalizarlas con una palabra específica, con longitudes entre 8 y 16 caracteres. Guarda, copia, utiliza y elimina contraseñas fácilmente con una interfaz intuitiva.",
+        "Crea y gestiona tus contraseñas de forma segura y sencilla. Generá contraseñas automáticas o personalizalas con palabras clave, entre 8 y 16 caracteres. Guardalas, copialas, usalas y eliminá lo que no necesites, todo desde una interfaz simple e intuitiva.",
       technologies: [
         "React",
         "CSS",
@@ -222,7 +222,7 @@ export const DATA = {
       dates: "Febrero 2024 - Febrero 2024",
       active: true,
       description:
-        "Aplicación destinada a la gestión y almacenamiento de tareas pendientes. Esta herramienta proporciona una organización efectiva y establece objetivos claros para avanzar de manera eficiente en el desarrollo y gestión de proyectos.",
+        "Aplicación para organizar, priorizar y avanzar en tus tareas con claridad. Diseñada para gestionar y almacenar tareas pendientes, esta herramienta te ayuda a estructurar tu trabajo, establecer objetivos claros y avanzar de forma eficiente en la ejecución de proyectos.",
       technologies: [
         "HTML",
         "CSS",
@@ -251,7 +251,7 @@ export const DATA = {
       dates: "Febrero 2023 - Febrero 2024",
       active: true,
       description:
-        "Aplicación que proporciona información meteorológica detallada para la ciudad consultada. La interfaz muestra datos clave, como el nombre de la ciudad, el país, la temperatura actual y una descripción del clima vigente. Además, la aplicación está integrada con Google Maps para facilitar la ubicación geográfica.",
+        "Consulta el clima de cualquier ciudad con datos precisos y visuales. La aplicación muestra información meteorológica detallada, incluyendo ciudad, país, temperatura actual y descripción del clima. Además, cuenta con integración con Google Maps para ubicar geográficamente cada consulta de forma rápida e intuitiva.",
       technologies: [
         "HTML",
         "CSS",
@@ -303,7 +303,7 @@ export const DATA = {
       dates: "Julio 2023 - Noviembre 2023",
       active: true,
       description:
-        "En mi último año de secundaria desarrollé un chatbot para responder diversas consultas relacionadas con la institución, como la historia del colegio, su administración, las modalidades de carreras técnicas de seis años y detalles sobre la Bienal (Exposición Educativa).",
+        "En mi último año de secundaria, creé un chatbot capaz de responder consultas frecuentes sobre el colegio: su historia, autoridades, modalidades de las carreras técnicas (6 años) y detalles de la Bienal, una exposición educativa destacada. Este proyecto buscó mejorar la comunicación institucional y ofrecer respuestas inmediatas a estudiantes y visitantes.",
       technologies: [
         "HTML",
         "CSS",
@@ -328,7 +328,7 @@ export const DATA = {
       dates: "Agosto 2023 - Noviembre 2023",
       active: true,
       description:
-        "El sitio web presenta los episodios emitidos de un podcast y ofrece a los usuarios la capacidad de gestionar una lista de favoritos, permitiendo agregar o eliminar capítulos. La funcionalidad de favoritos requiere que los usuarios creen una cuenta, que incluye la opción de personalizar la imagen de perfil.",
+        "Sitio web de un podcast con gestión de favoritos y perfil personalizado. La plataforma muestra los episodios publicados y permite a los usuarios crear una cuenta para marcar capítulos como favoritos. Cada usuario puede agregar o quitar episodios de su lista personal y personalizar su perfil, incluyendo la imagen de avatar.",
       technologies: [
         "HTML",
         "Javascript",
@@ -353,7 +353,7 @@ export const DATA = {
       dates: "Abril 2022 - Diciembre 2022",
       active: true,
       description:
-        "Proyecto de Arduino integrado con una aplicación web. La aplicación presenta preguntas predeterminadas de manera aleatoria y exhibe los puntajes individuales de los participantes en una pantalla LCD 16x16.",
+        "Proyecto de Arduino conectado con una aplicación web para mostrar puntajes en tiempo real. La aplicación web conectada presenta preguntas predeterminadas de forma aleatoria. A medida que los participantes responden, sus puntajes individuales se muestran en una pantalla LCD 16x2, generando una experiencia dinámica.",
       technologies: [
         "HTML",
         "Javascript",
